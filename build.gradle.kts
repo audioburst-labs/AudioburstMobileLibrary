6a3a54ace131362fd88137a5e474176237a82cc3
Kamil Halko <k.halko@nomtek.com>
2020-09-29 11:48:24 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    kotlin("multiplatform") version "1.4.10"
+    id("com.android.library")
+    id("kotlin-android-extensions")
+}
+group = "com.audioburst"
+version = "0.0.1"
+
+repositories {
+    gradlePluginPortal()
+    google()
+    jcenter()
+    mavenCentral()
+}
+kotlin {
+    android()
+    iosX64("ios") {
+        binaries {
+            framework {
+                baseName = "library"
+            }
+        }
+    }
+    js("browser") {
+        browser {
+            binaries.executable()
+            webpackTask {
+                cssSupport.enabled = true
+            }
+            runTask {
+                cssSupport.enabled = true
+            }
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    webpackConfig.cssSupport.enabled = true
+                }
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("androidx.core:core-ktx:1.2.0")
+            }
+        }
+        val androidTest by getting
+        val iosMain by getting
+        val iosTest by getting
+        val browserMain by getting
+        val browserTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+    }
+}
+android {
+    compileSdkVersion(29)
+    defaultConfig {
+        minSdkVersion(24)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+}
