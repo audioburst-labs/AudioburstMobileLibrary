959ece8f38a451e0558732eb296c22eb9cee5243
Kamil Halko <k.halko@nomtek.com>
2021-05-05 10:17:33 +0200

    Fix a problem with wrong timestamp on iOS

--- src/commonTest/kotlin/com/audioburst/library/utils/strategies/ListenedStrategyTest.kt
+++ src/commonTest/kotlin/com/audioburst/library/utils/strategies/ListenedStrategyTest.kt
@@ -4,13 +4,8 @@ import com.audioburst.library.interactors.advertisementOf
 import com.audioburst.library.interactors.burstOf
 import com.audioburst.library.interactors.downloadedAdvertisementOf
 import com.audioburst.library.interactors.playlistOf
-import com.audioburst.library.models.DurationUnit
-import com.audioburst.library.models.PlaybackEvent
-import com.audioburst.library.models.Url
-import com.audioburst.library.models.toDuration
-import com.audioburst.library.utils.InputBasedPlaybackPeriodsCreator
-import com.audioburst.library.utils.listenedStrategyOf
-import com.audioburst.library.utils.playbackStateOf
+import com.audioburst.library.models.*
+import com.audioburst.library.utils.*
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
@@ -100,5 +95,54 @@ class ListenedStrategyTest {
 
         // THEN
         assertTrue(events.filterIsInstance<PlaybackEvent.TwoSecADPlaying>().isNotEmpty())
+    }
+
+    @Test
+    fun testIfMultipleEventsAreGettingDetected() {
+        // GIVEN
+        val url = "https://storageaudiobursts.azureedge.net/audio/J8OoDzXD9Pwn.mp3"
+        val playlist = playlistOf(
+            bursts = listOf(
+                burstOf(
+                    audioUrl = url,
+                    duration = 48.0.toDuration(DurationUnit.Seconds)
+                )
+            )
+        )
+        val previousStates: Queue<InternalPlaybackState> = FixedSizeQueue(10)
+        val listenedStrategy = listenedStrategyOf(
+            creator = InputBasedPlaybackPeriodsCreator(),
+            factory = listenedMediaStrategyFactoryOf(
+                2.0.toDuration(DurationUnit.Seconds)
+            ),
+        )
+        val now = PlatformTimestampProvider.currentTimeMillis()
+
+        // WHEN
+        val events = listOf(
+            1.240, 3.243, 5.245, 7.247, 9.249, 11.252, 13.254, 15.256, 17.258, 19.260, 21.262, 23.264, 25.266, 27.268,
+            29.270, 31.272, 33.274, 35.276, 37.278, 39.280, 41.282, 43.285, 45.287,
+        ).flatMap { position ->
+            val input = inputOf(
+                currentState = playbackStateOf(
+                    url = url,
+                    position = position,
+                    occurrenceTime = now + (position * 1000).toLong()
+                ),
+                playlist = playlist,
+                previousStates = previousStates,
+            )
+            listenedStrategy.check(input).apply {
+                previousStates.add(input.currentState)
+            }
+        }
+
+        events.forEach {
+            println(it.actionName)
+        }
+        // THEN
+        assertTrue(events.filterIsInstance<PlaybackEvent.TwoSecPlaying>().isNotEmpty())
+        assertTrue(events.filterIsInstance<PlaybackEvent.BurstListened>().isNotEmpty())
+        assertTrue(events.filterIsInstance<PlaybackEvent.Playing>().size == 4)
     }
 }
--- src/iosMain/kotlin/com/audioburst/library/utils/PlatformTimestampProvider.kt
+++ src/iosMain/kotlin/com/audioburst/library/utils/PlatformTimestampProvider.kt
@@ -5,5 +5,5 @@ import platform.Foundation.timeIntervalSince1970
 
 internal actual object PlatformTimestampProvider : TimestampProvider {
 
-    override fun currentTimeMillis(): Long = NSDate().timeIntervalSince1970.toLong()
+    override fun currentTimeMillis(): Long = NSDate().timeIntervalSince1970.toLong() * 1000
 }
