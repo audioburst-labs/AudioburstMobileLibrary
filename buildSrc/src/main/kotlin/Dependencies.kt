4e4272cbf5f7aa9b578ccf095f04aa1a52db1b25
Kamil Halko <k.halko@nomtek.com>
2021-07-06 11:59:32 +0200

    Increase library version to 0.0.25

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,6 +1,6 @@
 object Constants {
     const val projectName = "AudioburstMobileLibrary"
-    const val projectVersion = "0.0.24"
+    const val projectVersion = "0.0.25"
 
     object Library {
         const val packageName = "com.audioburst"
