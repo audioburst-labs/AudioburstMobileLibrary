d0ed03fe921f782e879c7f1ea1c65ee2a2c3ad4e
Kamil Halko <k.halko@nomtek.com>
2021-07-09 16:23:23 +0200

    Increase library version to 0.0.28

--- src/commonMain/kotlin/com/audioburst/library/utils/LibraryConfigurationHolder.kt
+++ src/commonMain/kotlin/com/audioburst/library/utils/LibraryConfigurationHolder.kt
@@ -32,6 +32,6 @@ internal class LibraryConfigurationHolder(
     }
 
     companion object {
-        private const val LIBRARY_VERSION = "0.0.27"
+        private const val LIBRARY_VERSION = "0.0.28"
     }
 }
